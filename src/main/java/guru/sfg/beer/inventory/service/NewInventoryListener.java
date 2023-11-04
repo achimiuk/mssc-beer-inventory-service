018fb24a5f1b2f23fa6e96c6ee10e2f534d864ba
achimiuk <achimiuk@gmail.com>
2023-11-04 09:01:16 +0100

    processing New Inventory event
    - moving events and dto to beer.common package
    - adding NewInventoryListener with updating beer inventory
    - ignoring /target

--- src/main/java/guru/sfg/beer/inventory/service/NewInventoryListener.java
+++ src/main/java/guru/sfg/beer/inventory/service/NewInventoryListener.java
@@ -2,17 +2,32 @@ package guru.sfg.beer.inventory.service;
 
 import guru.sfg.beer.common.events.NewInventoryEvent;
 import guru.sfg.beer.inventory.service.config.JmsConfig;
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
+import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class NewInventoryListener {
 
+    private final BeerInventoryRepository beerInventoryRepository;
+
+    @Transactional
     @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
     public void onNewInventoryEvent(NewInventoryEvent newInventoryEvent) {
-        log.debug("New inventory QOH: " + newInventoryEvent.getBeerDto().getQuantityOnHand());
+        log.debug("New inventory requested QOH: " + newInventoryEvent.getBeerDto().getQuantityOnHand());
+
+        BeerInventory beerInventory = beerInventoryRepository.findByUpc(newInventoryEvent.getBeerDto().getUpc());
+
+        beerInventory.setQuantityOnHand(beerInventory.getQuantityOnHand() + newInventoryEvent.getBeerDto().getQuantityOnHand());
+
+        beerInventoryRepository.save(beerInventory);
+
     }
 
 }
